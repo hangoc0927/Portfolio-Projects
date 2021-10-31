de82b4b90d9ffb47ce94d20fdc0c7a85ea7d6a87
Ha Nguyen <ha.ngoc.0927@gmail.com>
2021-10-31 20:30:57 +1100

    Update Covid19_WorldNumbers.sql

--- Covid19_WorldNumbers.sql
+++ Covid19_WorldNumbers.sql
@@ -3,7 +3,7 @@ From PortfolioProjects..CovidDeaths
 Where continent is not null 
 order by 3,4
 
--- Select Data that we are going to be starting with
+-- Select Data to Start with
 
 Select Location, date, total_cases, new_cases, total_deaths, population
 From PortfolioProjects..CovidDeaths
@@ -12,7 +12,7 @@ order by 1,2
 
 
 -- Total Cases vs Total Deaths
--- Shows likelihood of dying if you contract covid in your country
+
 
 Select Location, date, total_cases,total_deaths, (total_deaths/total_cases)*100 as DeathPercentage
 From PortfolioProjects..CovidDeaths
@@ -22,7 +22,7 @@ order by 1,2
 
 
 -- Total Cases vs Population
--- Shows what percentage of population infected with Covid
+
 
 Select Location, date, Population, total_cases,  (total_cases/population)*100 as PercentPopulationInfected
 From PortfolioProjects..CovidDeaths
@@ -45,9 +45,6 @@ Where continent is not null
 Group by Location
 order by TotalDeathCount desc
 
-
-
--- BREAKING THINGS DOWN BY CONTINENT
 
 -- Showing contintents with the highest death count per population
 
